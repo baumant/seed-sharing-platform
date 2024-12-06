23cb712feab76941dee4c0ae9ccce1eeb57b4269
Tim Bauman <tebauman@gmail.com>
2024-12-06 16:19:01 -0500

    allow webp images

--- controllers/seedController.js
+++ controllers/seedController.js
@@ -17,7 +17,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "seeds",
-    allowed_formats: ["jpg", "png", "jpeg"],
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
 
